9a990955c623292898217285f1e330d45ca1c887
Stas Boukarev <stassats@gmail.com>
2013-02-28 02:49:44 +0400

    Better launch-browser.

--- launch-browser.lisp
+++ launch-browser.lisp
@@ -1,9 +1,25 @@
 (in-package #:qt-ui)
 (named-readtables:in-readtable :qt)
 
-(defun launch-browser (url)
-  #+sbcl(sb-ext:run-program "opera" (list url)
-                            :search t
-                            :wait nil)
-  #+ccl(ccl:run-program "opera" (list url)
-                        :wait nil))
+(defun make-url (url parameters)
+  (with-output-to-string (str)
+    (write-string url str)
+    (loop for delimiter = #\? then #\&
+          for (key value) on parameters by #'cddr
+          do
+          (write-char delimiter str)
+          (write-string (drakma:url-encode (if (symbolp key)
+                                               (string-downcase key)
+                                               key)
+                                           :utf-8)
+                        str)
+          (write-char #\= str)
+          (write-string (drakma:url-encode value :utf-8) str))))
+
+(defun launch-browser (url &rest parameters &key &allow-other-keys)
+  (let ((url (make-url url parameters)))
+    #+sbcl(sb-ext:run-program "opera" (list url)
+                              :search t
+                              :wait nil)
+    #+ccl(ccl:run-program "opera" (list url)
+                          :wait nil)))
